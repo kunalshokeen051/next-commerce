fb4668d5065b12b55bf0770212f30663cac0b359
kunalshokeen051 <119399583+kunalshokeen051@users.noreply.github.com>
2023-06-27 11:53:08 +0530

    babel file added

--- pages/contact.js
+++ pages/contact.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import Wrapper from '@/components/Wrapper'
 
 const contact = () => {
-  return (
-    <div className='h-screen flex flex-col gap-10 p-10 px-52'>
-      <div className="">
-        <h1 className='text-8xl text-black font-urbanist font-extrabold'>Contact Us</h1>
-        <div className="w-[500px] border-2 border-rose-700 "></div>
-      </div>
-      <div className="max-w-screen border-2 rounded-md h-12 border-gray-400">
-       <form action="/">
-      
-       </form>
-      </div>
-    </div>
-  )
+    return (
+        <div className='w-full h-[calc(100vh-82px)]'>
+            <Wrapper className={'border-2 w-full my-4 mx-2 h-full rounded-md border-slate-500'}>
+                <h1 className='m-4 text-7xl'>Contact Us</h1>
+                <form action="">
+                   <div className="flex gap-2">
+                   <div name='name' className="w-1/2 h-fit border-2">
+                         <h3>Name</h3>
+                        <input type="text" name='name' className='border-2 border-rose-500 w-full' />
+                    </div>
+                    <div name='name' className="w-1/2 border-2">
+                        <input type="text" name='name' />
+                    </div>
+                   </div>
+                </form>
+            </Wrapper>
+        </div>
+    )
 }
 
 export default contact
